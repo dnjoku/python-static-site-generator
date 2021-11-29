01116f7d62117f59576458492a7d9a3c9105b5ea
Dan Njoku <dnjoku@ups.com>
2021-11-29 17:42:34 -0500

    Module Content Class: fixed typo

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -45,8 +45,9 @@ class MarkdownParser(Parser):
         self.write(path, dest, html)
         sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
 
+
 class ReStructuredTextParser(Parser):
-    extentions = [".rst"]
+    extensions = [".rst"]
 
     def parse(self, path, source, dest):
         content = Content.load(self.read(path))
