f175b4aaed68fb5361a79ce7f7d84a499b6e55e1
Dan Njoku <dnjoku@ups.com>
2021-11-29 17:34:57 -0500

    Module Content Class: making progress

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -1,7 +1,12 @@
 import shutil
+import sys
 
 from typing import List
 from pathlib import Path
+
+from docutils.core import publish_parts
+from markdown import markdown
+from ssg.content import Content
 
 
 class Parser:
@@ -30,3 +35,21 @@ class ResourceParser(Parser):
 
     def parse(self, path, source, dest):
         self.copy(path, source, dest)
+
+class MarkdownParser(Parser):
+    extension = [".md", ".markdown"]
+
+    def parse(self, path, source, dest):
+        content = Content.load(self.read(path))
+        html = markdown(content.body)
+        self.wrtie(path, dest, html)
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
+
+class ReStructuredTextParser(Parser):
+    extention = [".rst"]
+
+    def parse(self, path, source, dest):
+        content = Content.load(self.read(path))
+        html = publish_parts(content.body, writer_name="html5")
+        self.wrtie(path, dest, html["html_body"])
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
